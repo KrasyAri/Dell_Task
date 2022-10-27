ed7b55146f273f7ffed72c0e958d35fd6340571c
Ivan Kirilov <vankata.kirilov@gmail.com>
2022-10-27 15:10:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Krasimira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbs = { 1, 1, 0, 1, 1, 1, 0, 0, 1 };
+
+            List<int> currentCounter = new List<int>();
+
+            int counter = 0;
+
+            for (int i = 0; i < numbs.Length; i++)
+            {
+                if(numbs[i] == 1)
+                {
+                    counter++;
+                }
+                else
+                {
+                    currentCounter.Add(counter);
+                    counter = 0;
+                }
+            }
+
+            currentCounter.Sort();
+            int lastElement = currentCounter[currentCounter.Count - 1];
+           
+
+            Console.WriteLine(lastElement);
+
+        }
+    }
+}
